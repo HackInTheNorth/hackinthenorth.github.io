6a22a03ffe34cd89b039e3437fcaf5c2ebdda536
Sahil Hindwani <hindwanisahil@yahoo.in>
2016-04-02 21:27:10 +0530

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,9 +1,5 @@
 var projects = {
-	"demo":{
-		"image":"images/1.png",
-		"team":"hitn team",
-		"link":"https://github.com/HackInTheNorth/hackinthenorth.github.io"
-	},
+	
 	"undefined":{
 		"image":"images/404",
 		"team":"undefined",
