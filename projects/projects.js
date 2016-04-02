e5305449dba8eacdf98c21eb6d71d326ae251dd6
utk3995 <iit2014507@iiita.ac.in>
2016-04-02 18:37:01 +0530

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -4,4 +4,10 @@ var projects = {
 		"team":"hitn team",
 		"link":"https://github.com/HackInTheNorth/hackinthenorth.github.io"
 	}
+	
+	"mediquik":{
+		"image":"",
+		"team":"softvokers",
+		"link":"https://github.com/utk3995/mediquik-"
+	}
 }
