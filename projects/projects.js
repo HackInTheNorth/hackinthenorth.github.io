39eb7e1c60a79e5f119ef8f3f33f925f1b0a1ac6
Rajat Saxena <rajat8171@gmail.com>
2016-04-03 12:13:51 +0530

    Updated projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -5,7 +5,7 @@ var projects = {
 		"link":"https://github.com/HackInTheNorth/hackinthenorth.github.io"
 	}
 	"Tracks":{
-		"image":" ",
+		"image":"/images/Tracks1.png",
 		"team":"We-The_Beer",
 		"link":"https://github.com/srajat/Tracks/"
 	}
