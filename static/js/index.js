f555fdf452a5bc6e009e0b8523801331e50bab33
pradyumna1 <contactpradyumna1@gmail.com>
2016-10-21 13:36:39 +0530

    Added jquery to latest version and added proper modal boxes

--- static/js/index.js
+++ static/js/index.js
@@ -1,95 +1,63 @@
-
-$(document).ready(function(){
-$(".dropdownbutton").click(function(){
-	if($("ul.form").hasClass("active")){
-		$(".main-body").css("padding-left","0px");
-		$("ul.form").removeClass("active");
-	}
-	else
-	{
-		$(".main-body").css("padding-left","30px");
-		$("ul.form").addClass("active");	
-	}
-	console.log("hello world");
-
-});
-  $(document).on("scroll", onScroll);
-  $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-      
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-
-function onScroll(event){
+function onScroll(event) {
     var scrollPos = $(document).scrollTop();
-    $('.nav-buttons a').each(function () {
+    $('.nav-buttons a').each(function() {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
             $('.nav-buttons a').removeClass("active");
             currLink.addClass("active");
-        }
-        else{
+        } else {
             currLink.removeClass("active");
         }
     });
 }
-    max_scroll=($(".landing-footer").position()).top+$(".landing-footer").outerHeight()-$(".nav-buttons").height();
+$(document).ready(function() {
+    $(".dropdownbutton").click(function() {
+        if ($("ul.form").hasClass("active")) {
+            $(".main-body").css("padding-left", "0px");
+            $("ul.form").removeClass("active");
+        } else {
+            $(".main-body").css("padding-left", "30px");
+            $("ul.form").addClass("active");
+        }
+        console.log("hello world");
+
+    });
+    $(document).on("scroll", onScroll);
+    // $('a[href^="#"]').on('click', function(e) {
+    //     e.preventDefault();
+    //     $(document).off("scroll");
+
+    //     $('a').each(function() {
+    //         $(this).removeClass('active');
+    //     })
+    //     $(this).addClass('active');
+
+    //     var target = this.hash,
+    //         menu = e.target;
+    //     $target = $(e.target);
+    //     $('html, body').stop().animate({
+    //         'scrollTop': $target.offset().top + 2
+    //     }, 500, 'swing', function() {
+    //         window.location.hash = target;
+    //         $(document).on("scroll", onScroll);
+    //     });
+    // });
+
+    max_scroll = ($(".landing-footer").position()).top + $(".landing-footer").outerHeight() - $(".nav-buttons").height();
     //console.log(($(".nav-buttons").position()).top);
-     $(window).scroll(function () {
+    $(window).scroll(function() {
         var navbar = $(".nav-buttons");
 
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        if(scrollTop > max_scroll && !navbar.is(".navbuttonFixed")) {
-                navbar.addClass("navbuttonFixed");
-                 $(".main-header")
+        if (scrollTop > max_scroll && !navbar.is(".navbuttonFixed")) {
+            navbar.addClass("navbuttonFixed");
+            $(".main-header")
+        } else if (scrollTop < max_scroll && navbar.is(".navbuttonFixed")) {
+            navbar.removeClass("navbuttonFixed");
         }
-        else if(scrollTop < max_scroll && navbar.is(".navbuttonFixed") ) {
-                navbar.removeClass("navbuttonFixed");
-        }
 
-});
-
-
-/* 
-        Modals start 
-*/
-
-var collegeAmbassadorModal = document.getElementById('college-ambassador-modal');
-var collegeAmbassadorButton = document.getElementById('college-ambassador-button');
-var collegeAmbassadorClose = document.getElementsByClassName("close")[0];
-
-collegeAmbassadorButton.onclick = function() {
-    collegeAmbassadorModal.style.display = "block";
-}
-
-collegeAmbassadorClose.onclick = function() {
-    collegeAmbassadorModal.style.display = "none";
-}
-
-window.onclick = function(event){
-    if(event.target == collegeAmbassadorModal){
-        collegeAmbassadorModal.style.display = "none";
-    }
-}
-
-/* 
-        Modals end 
-*/
+    });
 
 
 });
