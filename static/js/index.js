35972d34aee441e780b3bda9cc34ee7718e4569f
pradyumna1 <contactpradyumna1@gmail.com>
2016-10-25 00:47:56 +0530

    Added nav-bar, updated font, added font-icon, made more responsive

--- static/js/index.js
+++ static/js/index.js
@@ -1,61 +1,62 @@
-function onScroll(event) {
-    var scrollPos = $(document).scrollTop();
-    $('.nav-buttons a').each(function() {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('.nav-buttons a').removeClass("active");
-            currLink.addClass("active");
-        } else {
-            currLink.removeClass("active");
-        }
-    });
+function scrollTo() {
+    var $el = $(this),
+        id = $el.attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 500);
+
+    return false;
 }
+
 $(document).ready(function() {
-    $(".dropdownbutton").click(function() {
-        if ($("ul.form").hasClass("active")) {
-            $(".main-body").css("padding-left", "0px");
-            $("ul.form").removeClass("active");
-        } else {
-            $(".main-body").css("padding-left", "30px");
-            $("ul.form").addClass("active");
+    var sections = $('.section'),
+        nav = $('nav'),
+        nav_height = nav.outerHeight(),
+        sponsorsSecondLink = $('#sponsor-link2');
+
+    /*updates active link according to current location, if the URL has some hash link to some section*/
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+        var top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            if ($(this).attr('id') != 'home') {
+                nav.addClass('not-landing');
+            } else {
+                nav.removeClass('not-landing');
+            }
         }
-        console.log("hello world");
+
 
     });
-    $(document).on("scroll", onScroll);
-    // $('a[href^="#"]').on('click', function(e) {
-    //     e.preventDefault();
-    //     $(document).off("scroll");
 
-    //     $('a').each(function() {
-    //         $(this).removeClass('active');
-    //     })
-    //     $(this).addClass('active');
 
-    //     var target = this.hash,
-    //         menu = e.target;
-    //     $target = $(e.target);
-    //     $('html, body').stop().animate({
-    //         'scrollTop': $target.offset().top + 2
-    //     }, 500, 'swing', function() {
-    //         window.location.hash = target;
-    //         $(document).on("scroll", onScroll);
-    //     });
-    // });
+    $(window).on('scroll', function() {
+        var cur_pos = $(this).scrollTop();
 
-    // max_scroll = ($(".landing-footer").position()).top + $(".landing-footer").outerHeight() - $(".nav-buttons").height();
-    // //console.log(($(".nav-buttons").position()).top);
-    // $(window).scroll(function() {
-    //     var navbar = $(".nav-buttons");
+        sections.each(function() {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
 
-    //     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    //     if (scrollTop > max_scroll && !navbar.is(".navbuttonFixed")) {
-    //         navbar.addClass("navbuttonFixed");
-    //         $(".main-header")
-    //     } else if (scrollTop < max_scroll && navbar.is(".navbuttonFixed")) {
-    //         navbar.removeClass("navbuttonFixed");
-    //     }
-
-    // });
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+                if ($(this).attr('id') != 'home') {
+                    nav.addClass('not-landing');
+                } else {
+                    nav.removeClass('not-landing');
+                }
+            }
+        });
+    });
+    nav.find('a').on('click', scrollTo);
+    sponsorsSecondLink.on('click', scrollTo);
+    // $('.subscribe-box btn1').on('click', )
 });
