7759346ef83508b94fb5780b2e7c181b2e58d310
Yash Sharma <yasharmaster@gmail.com>
2016-11-17 19:18:08 +0530

    Add team page navbar

--- static/js/team.js
+++ static/js/team.js
@@ -1,3 +1,23 @@
 $( document ).ready(function() {
 	$('#cardframe').mixItUp();
+
+	$(".button").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#cardframe").offset().top - 60},
+        'slow');
+	});
+
+	$(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+
 });
