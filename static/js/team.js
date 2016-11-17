ebb5a2e5ac985391e90534ed178a71efecf33b67
pradyumna1 <contactpradyumna1@gmail.com>
2016-11-18 01:05:32 +0530

    smoothed the navbar's sticky behavior

--- static/js/team.js
+++ static/js/team.js
@@ -1,23 +1,25 @@
-$( document ).ready(function() {
-	$('#cardframe').mixItUp();
+$(document).ready(function() {
+    $('#cardframe').mixItUp();
 
-	$(".button").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#cardframe").offset().top - 60},
-        'slow');
-	});
-
-	$(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-      console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 280) {
-      $('#nav_bar').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < 281) {
-      $('#nav_bar').removeClass('navbar-fixed');
-    }
-  });
+    $(".button").click(function() {
+        $('html,body').animate({
+                scrollTop: $("#cardframe").offset().top - 60
+            },
+            'slow');
+    });
+    var navBar = $('#nav_bar');
+    var navBarPosition = navBar.offset().top;
+    $(window).scroll(function() {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+        if ($(window).scrollTop() > navBarPosition) {
+            $('#nav_bar').addClass('navbar-fixed');
+        }
+        if ($(window).scrollTop() < navBarPosition) {
+            $('#nav_bar').removeClass('navbar-fixed');
+        }
+    });
 
 });
