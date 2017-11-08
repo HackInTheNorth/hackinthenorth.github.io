022a9411145a2a04d5cc3656993d22660abfc3fb
Anupam-dagar <dagaranupam@gmail.com>
2017-11-09 01:36:41 +0530

    finally faq jerk is fixed now, new code for js written.

---
+++ main/static/js/faq.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	$("#box1").slideToggle();
+	$("#box2").slideToggle();
+	$("#box3").slideToggle();
+	$("#box4").slideToggle();
+	$("#box5").slideToggle();
+	$("#box6").slideToggle();
+	$("#box7").slideToggle();
+	$("#box8").slideToggle();
+	$("#ques1").click(function(){
+		$("#faq1").css("display","block");
+		$("#box1").slideToggle();
+	});
+
+	$("#ques2").click(function(){
+		$("#faq2").css("display","block");
+		$("#box2").slideToggle();
+	});
+
+	$("#ques3").click(function(){
+		$("#faq3").css("display","block");
+		$("#box3").slideToggle();
+	});
+
+	$("#ques4").click(function(){
+		$("#faq4").css("display","block");
+		$("#box4").slideToggle();
+	});
+
+	$("#ques5").click(function(){
+		$("#faq5").css("display","block");
+		$("#box5").slideToggle();
+	});
+
+	$("#ques6").click(function(){
+		$("#faq6").css("display","block");
+		$("#box6").slideToggle();
+	});
+
+	$("#ques7").click(function(){
+		$("#faq7").css("display","block");
+		$("#box7").slideToggle();
+	});
+
+	$("#ques8").click(function(){
+		$("#faq8").css("display","block");
+		$("#box8").slideToggle();
+	});
+});
