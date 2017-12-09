129a620bacec3bade2bf5b9fe899d321906ef53c
OrionStar25 <chunnushrivastava@gmail.com>
2017-12-10 03:07:21 +0530

    gallery

---
+++ static/js/slideshow.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $(".gallery-img").click(function(){
+    var t = $(this).attr("src");
+    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+    $("#myModal").modal();
+  });
+});
