4278a379839daac22dc67af0d1cc0aacb535b1fd
OrionStar25 <chunnushrivastava@gmail.com>
2017-12-10 15:34:35 +0530

    restore site

--- static/js/slideshow.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $(".gallery-img").click(function(){
-    var t = $(this).attr("src");
-    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-    $("#myModal").modal();
-  });
-});
